393e848a7d51bec2a53f6e172afc298d75d53f08
Valentyn Riabokonenko <valentyn_riabokonenko@epam.com>
2017-03-28 14:05:21 +0200

    Initial structure

---
+++ src/main/java/com/github/sane88/githubintegration/GithubIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.github.sane88.githubintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIntegrationApplication.class, args);
+	}
+}
